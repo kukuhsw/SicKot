ef87d97def8dea907794a4b76957a86db0484d04
kukuhsw <kukuhsatriowibowo@gmail.com>
2020-09-03 10:55:40 +0700

    add icon search

--- app/src/main/java/com/hellu/lekot/activities/ProfileActivity.kt
+++ app/src/main/java/com/hellu/lekot/activities/ProfileActivity.kt
@@ -1,7 +1,14 @@
 package com.hellu.lekot.activities
 
+import android.graphics.Outline
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
 import com.hellu.lekot.R
 
 class ProfileActivity : AppCompatActivity() {
@@ -12,6 +19,24 @@ class ProfileActivity : AppCompatActivity() {
         val actionbar = supportActionBar
         actionbar!!.title = "Profile"
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        val image = findViewById<CardView>(R.id.cardView)
+        val curveRadius = 40F
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            image.outlineProvider = object : ViewOutlineProvider() {
+
+                @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                override fun getOutline(view: View?, outline: Outline?) {
+                    outline?.setRoundRect(0, 0, view!!.width, (view.height+curveRadius).toInt(), curveRadius)
+                }
+            }
+
+            image.clipToOutline = true
+
+        }
+
 
     }
 
