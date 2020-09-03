8f3e2733e12337ae40f87426540aa1a39a65a909
kukuhsw <kukuhsatriowibowo@gmail.com>
2020-09-03 07:05:34 +0700

    recolor,refont and refix layout

--- app/src/main/java/com/hellu/lekot/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/hellu/lekot/activities/SplashscreenActivity.kt
@@ -1,11 +1,17 @@
 package com.hellu.lekot.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextUtils
+import android.text.style.ForegroundColorSpan
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import com.hellu.lekot.R
 
 class SplashscreenActivity : AppCompatActivity() {
