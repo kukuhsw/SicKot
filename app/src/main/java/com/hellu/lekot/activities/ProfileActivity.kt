90a3c4e14cdc257b3cbdcb663cb91450a6e22fc4
kukuhsw <kukuhsatriowibowo@gmail.com>
2020-09-05 12:13:30 +0700

    implement function showcard and show photoprofile

--- app/src/main/java/com/hellu/lekot/activities/ProfileActivity.kt
+++ app/src/main/java/com/hellu/lekot/activities/ProfileActivity.kt
@@ -19,19 +19,18 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
         supportActionBar?.let {
             it.title = "Profile"
             it.setDisplayHomeAsUpEnabled(true)
         }
-
+        val curveRadius = 40F
         val imgProfile = findViewById<CircleImageView>(R.id.profile_image)
+        val image = findViewById<CardView>(R.id.cardView)
+
         val cardSkill = findViewById<ExpandableCardView>(R.id.card_skill)
         val cardCertificate = findViewById<ExpandableCardView>(R.id.card_certificate)
         val cardAward = findViewById<ExpandableCardView>(R.id.card_award)
         val cardHoby = findViewById<ExpandableCardView>(R.id.card_hoby)
-        val image = findViewById<CardView>(R.id.cardView)
-        val curveRadius = 40F
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             image.outlineProvider = object : ViewOutlineProvider() {
@@ -43,22 +42,42 @@ class ProfileActivity : AppCompatActivity() {
             image.clipToOutline = true
         }
 
+        showPhotoProfile(imgProfile)
+        showCardSkill(cardSkill)
+        showCardCertificate(cardCertificate)
+        showCardAward(cardAward)
+        showCardHoby(cardHoby)
+
+    }
+
+    private fun showPhotoProfile(imgProfile: CircleImageView?) {
         val url = "https://media-exp1.licdn.com/dms/image/C5603AQGujT6VG_WVew/profile-displayphoto-shrink_800_800/0?e=1604534400&v=beta&t=YYRTJZ9i0cd3IMAnvTPWLyPN7x97LbaNWnsHc5qtPd0"
-        Glide.with(this).load(url).placeholder(R.mipmap.ic_launcher).fitCenter().dontAnimate().into(imgProfile);
+        if (imgProfile != null) {
+            Glide.with(this).load(url).placeholder(R.mipmap.ic_launcher).fitCenter().dontAnimate().into(imgProfile)
+        };
+    }
 
-        cardSkill.cardTitle = "Skill"
-        cardSkill.cardDescription = "Kotlin Java"
-        cardSkill.setBackgroundColor(Color.parseColor("#9796f0"))
-        cardCertificate.cardTitle = "Certificate"
-        cardCertificate.cardDescription = "IoT Architect, Flutter Expert, Golang Advance, Pengembangan ML"
-        cardCertificate.setBackgroundColor(Color.parseColor("#ffcc33"))
-        cardAward.cardTitle = "Award"
-        cardAward.cardDescription = "Mawapres Vokasi IPB, 1st Thinkubator"
-        cardAward.setBackgroundColor(Color.parseColor("#fd746c"))
-        cardHoby.cardTitle = "Hoby"
-        cardHoby.cardDescription = "Basket, Badminton, Swimming"
-        cardHoby.setBackgroundColor(Color.parseColor("#B3FFAB"))
+    private fun showCardHoby(mCardHoby: ExpandableCardView) {
+        mCardHoby.cardTitle = "Hoby"
+        mCardHoby.cardDescription = "Basket, Badminton, Swimming"
+        mCardHoby.setBackgroundColor(Color.parseColor("#B3FFAB"))
+    }
 
+    private fun showCardAward(mCardAward: ExpandableCardView) {
+        mCardAward.cardTitle = "Award"
+        mCardAward.cardDescription = "Mawapres Vokasi IPB, 1st Thinkubator"
+        mCardAward.setBackgroundColor(Color.parseColor("#fd746c"))    }
+
+    private fun showCardCertificate(mCardCertificate: ExpandableCardView) {
+        mCardCertificate.cardTitle = "Certificate"
+        mCardCertificate.cardDescription = "IoT Architect, Flutter Expert, Golang Advance, Pengembangan ML"
+        mCardCertificate.setBackgroundColor(Color.parseColor("#ffcc33"))
+    }
+
+    private fun showCardSkill(mCardSkill: ExpandableCardView) {
+        mCardSkill.cardTitle = "Skill"
+        mCardSkill.cardDescription = "Kotlin Java"
+        mCardSkill.setBackgroundColor(Color.parseColor("#9796f0"))
     }
 
     override fun onSupportNavigateUp(): Boolean {
