e7b4fba9ea9483e92f9d179fcede7ade3b7e9228
kukuhsw <kukuhsatriowibowo@gmail.com>
2020-09-14 18:08:47 +0700

    add btn share whatsapp

--- app/src/main/java/com/hellu/lekot/activities/DetailPlanetActivity.kt
+++ app/src/main/java/com/hellu/lekot/activities/DetailPlanetActivity.kt
@@ -1,8 +1,15 @@
 package com.hellu.lekot.activities
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -29,6 +36,7 @@ class DetailPlanetActivity : AppCompatActivity() {
         val tvTemp: TextView = findViewById(R.id.desc_text_temp)
         val tvDiameter: TextView = findViewById(R.id.desc_text_diameter)
         val tvWeight: TextView = findViewById(R.id.desc_text_weight)
+        val btnShare: Button = findViewById(R.id.btn_share)
 
 
         val people = intent.getSerializableExtra("planet") as? Planet
@@ -54,6 +62,35 @@ class DetailPlanetActivity : AppCompatActivity() {
         tvDiameter.text = "diameter: $descDiameter"
         tvWeight.text = "weight: $descWeight"
 
+        btnShare.setOnClickListener {
+            shareToWhatsApps(people?.photo, pName)
+        }
+
+    }
+
+    private fun shareToWhatsApps(photo: Int?, pName: String) {
+        var imageUri: Uri? = null
+        try {
+            imageUri = Uri.parse(
+                MediaStore.Images.Media.insertImage(
+                    contentResolver,
+                    photo?.let { BitmapFactory.decodeResource(resources, it) }, null, null
+                )
+            )
+        } catch (e: NullPointerException) {
+            Toast.makeText(this, "Gambar tidak bisa di share", Toast.LENGTH_SHORT).show()
+        }
+        val whatsappIntent = Intent(Intent.ACTION_SEND)
+        whatsappIntent.type = "/"
+        whatsappIntent.putExtra(Intent.EXTRA_STREAM, imageUri)
+        whatsappIntent.putExtra(Intent.EXTRA_TEXT, pName)
+        whatsappIntent.setPackage("com.whatsapp")
+
+        try {
+            startActivity(whatsappIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, "Whatsapp belum di-install.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
